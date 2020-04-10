8ea0aed15f07c1c010bdd4a42534f6628eff9107
haekal803 <fikrihaekl@gmail.com>
2020-04-10 20:44:11 +0800

    Memanggil method dari Class dipackage lain

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
-﻿public class main{
+﻿import koneksi.Database;
+
+public class main{
  public static void main(String [] args){
   System.out.println("Ini program main");
+  Database.hubungkan();
  }
 }
