3bc72ed832814d6e9d267cd39022e753efee1395
Haekal803 <62798147+Haekal803@users.noreply.github.com>
2020-04-10 23:34:34 +0800

    c:\users\acer win 8\pertemuan02\pbo1-pertemuan02\koneksi\model\InputOutput.java

--- koneksi/model/InputOutput.java
+++ koneksi/model/InputOutput.java
@@ -4,8 +4,19 @@ public class InputOutput{
  public static void main (string[] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
+  int jumlahSks;
+  double IPK;
   System.out.print("Masukan nama anda: ");
   nama = scanner.nextline();
   System.out.println("Hai. "+nama);
+
+  System.out.print("Masukan jumlah sks yang sudah di tempuh:
+  jumlahSks = scanner.nextint();
+  
+  System.out.print("Masukan IPK sementara :");
+  IPK =scanner.nextdouble();
+
+  double jumlah = IPK * jumlahSks;
+  System.out.print("total nilai = "+jumlah); 
  }
 }
